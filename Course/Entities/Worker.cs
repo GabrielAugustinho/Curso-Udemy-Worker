c6f65d0dd62b6f95ee937f24da4971e1e3db2499
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-04-16 13:33:38 -0300

    Classe 'Departament' criada nela contém o atributo 'Name' e os construtores de resgate da mesma. Classe 'HourContract' criada para informar os contratos desse trabalhador, foi adicionado os atributos 'Date, ValuePerHour, Hours' e também foi criado os contrutores e o método que calcula o valor total a ser resgatado 'TotalValue()'. Foi criada uma classe de enumeração 'WorkerLevel' que informa o nível do trabalhador. Por fim foi criada a classe principal do trabalhador 'Worker' interligando os outros objetos a ela, foram criados os métodos de adicionar, remover na lista 'contracts' e por fim o médoto 'Income', esse metodo calcula o valor total do salário a receber baseado no mês e no ano dos contratos adicionados pelo usuário

---
+++ Course/Entities/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities.Enums;
+
+namespace Course.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contract { get; set; } = new List<HourContract>();
+
+        public Worker() { }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+    }
+}
